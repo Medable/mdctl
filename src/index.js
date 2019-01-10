dcdda9495f7cd802f8c647d4d6bd3c65cc22eed0
james <james@medable.com>
2019-01-10 13:43:55 -0800

    [package] boilerplate

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const defaultAwesomeFunction = (name) => {
+
+        const returnStr = `I am the Default Awesome Function, fellow comrade! - ${name}`
+
+        return returnStr
+      },
+      awesomeFunction = () => 'I am just an Awesome Function'
+
+export default defaultAwesomeFunction
+
+export { awesomeFunction }
