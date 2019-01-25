8765b681d96ff834a4337ee01a0f4cb173515613
Nico <nico@medable.com>
2019-01-25 14:47:34 -0300

    wip, refactoring accept method

--- src/cli/lib/manifest.js
+++ src/cli/lib/manifest.js
@@ -1,126 +1,125 @@
-// let manifest
-//
-// // blank manifest
-// manifest = {
-//   includes: ['*']
-// }
-//
-// manifest = {
-//
-//   // global includes
-//   includes: [
-//     '/^axon__nucleus_/'
-//   ],
-//
-//   // global excludes
-//   excludes: [
-//     '/axon__nucleus_debug/i'
-//   ],
-//
-//   // org environment configuration
-//   env: {
-//
-//     // exportable or custom properties (org extension properties, account extension properties)
-//     smsNumbers: {
-//       includes: ['*']
-//     },
-//
-//     // these are here for completeness but will be culled from output if empty
-//     apps: [],
-//     serviceAccounts: [],
-//     policies: [],
-//     roles: [],
-//     storage: [],
-//     notifications: [],
-//
-//     // org env configuration
-//     configuration: [{
-//       includes: [
-//         '*'
-//       ],
-//       excludes: [
-//         'minPasswordScore'
-//       ]
-//     }]
-//   },
-//
-//   objects: {
-//
-//     // which objects to include?
-//     includes: ['*'],
-//     excludes: ['/axon__nucleus_debug/i'],
-//
-//     //
-//     propertyIncludes: [
-//       '*'
-//     ],
-//     propertyExcludes: [
-//       '/debug/i',
-//       '/axon__doc_array/'
-//     ],
-//
-//     // object properties to include
-//     properties: [{
-//       name: 'org',
-//       includes: [
-//         '/^axon__/'
-//       ],
-//       excludes: [
-//         '/debug/i',
-//         '/axon__doc_array/'
-//       ]
-//     }, {
-//       name: 'axon__thing'
-//     }]
-//   },
-//
-//   scripts: {
-//
-//     // additive includes to top-level
-//     includes: [
-//       '/^c_test_script/'
-//     ],
-//
-//     // if you have to make an explicit exclude to counter additive includes
-//     excludes: [
-//       '/axon__nucleus_debug/i'
-//     ],
-//   },
-//
-//   views: {
-//
-//   },
-//
-//   templates: {
-//
-//   }
-//
-// }
 
 const { privatesAccessor } = require('../../utils/privates'),
       { validateRegex, rArray } = require('../../utils/values'),
-      { throwIfNot } = require('../../utils')
+      { throwIfNot } = require('../../utils'),
+      _ = require('lodash')
 
 
-class Manifest {
+// Augmented regular expresions. Accepts strings, star
+class ARegex {
 
-  constructor(spec) {
+  constructor(input) {
+    if (_.isString(input)) {
+      this.value = /^\//.test(input) && /\/$/.test(input)
+        ? new RegExp(input.substring(1,input.length-1))
+        : input
+    }
+    if (_.isRegExp(input)) {
+      this.value = input
+    }
+  }
 
-    const includes = rArray((spec && spec.includes) || ['*'], true),
-          excludes = rArray((spec && spec.excludes) || [], true)
+  test(pattern) {
+    if (_.isString(this.value)) {
+      return this.value === '*' || _.isEqual(pattern, this.value)
+    } else if (_.isRegExp(this.value)) {
+      return this.value.test(pattern)
+    }
 
-    includes.every(
-      value => throwIfNot('Some global include is not a regular expression', validateRegex(value, true))
-    )
+    return false
+  }
+}
 
-    excludes.every(
-      value => throwIfNot('Some global exclude is not a regular expression', validateRegex(value, true))
-    )
+// Basic matching stage
+class ManifestStage {
 
-    Object.assign(privatesAccessor(this), { includes, excludes })
+  constructor(def) {
+    def = def || {}
 
+    if (!def.includes) {
+      def.includes = ['*']
+    }
+
+    this.includes = rArray(def.includes || [], true).map(v => new ARegex(v))
+    this.excludes = rArray(def.excludes || [], true).map(v => new ARegex(v))
+  }
+
+  accept(path) {
+    const [last, ...prefix] = path.split('.').reverse(),
+      [first, ...rest] = path.split('.')
+
+    return this.includes.some(r => r.test(last))
+      && !this.excludes.some(r => r.test(last))
   }
 
 }
 
-module.exports = Manifest
+
+class ObjectManifest {
+
+  constructor(def) {
+    def = def || {}
+
+    if (!def.includes) {
+      def.includes = ['.*']
+    }
+
+    if (!def.name) {
+      throw new Error('Invalid Argument')
+    }
+
+    this.name = /^\//.test(def.name) && /\/$/.test(def.name)
+      ? def.name.substring(1,def.name.length-1)
+      : def.name
+    this.includes = rArray(def.includes || [], true)
+    this.excludes = rArray(def.excludes || [], true)
+  }
+
+  accept(path) {
+    const [last, ...prefix] = path.split('.').reverse(),
+          [first] = path.split('.')
+
+    if (this.name) return new RegExp(this.name).test(first)
+
+    return this.includes.some(r => new RegExp(r).test(last))
+      && !this.excludes.some(r => new RegExp(r).test(last))
+  }
+
+}
+
+
+class Manifest extends ManifestStage {
+
+  constructor(def) {
+    def = def || {}
+
+    super(def)
+    //
+    // if (!def.includes) {
+    //   def.includes = ['.*']
+    // }
+    //
+    // if (def.objects) {
+    //   this.objects = def.objects.map(section => new ObjectManifest(section))
+    // }
+    //
+    // this.includes = rArray(def.includes || [], true)
+    // this.excludes = rArray(def.excludes || [], true)
+  }
+
+  // accept(path) {
+  //   const [last, ...prefix] = path.split('.').reverse(),
+  //         [first, ...rest] = path.split('.')
+  //
+  //   if (this[first]) {
+  //     return this[first].some(section => section.accept(...rest))
+  //   }
+  //
+  //   return this.includes.some(r => new RegExp(r).test(last))
+  //     && !this.excludes.some(r => new RegExp(r).test(last))
+  // }
+
+
+}
+
+module.exports = { Manifest, ARegex }
