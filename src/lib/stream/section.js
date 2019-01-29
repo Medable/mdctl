8254715ceb003f412f48579134ac1f9cdd8d84fd
Gaston Robledo <gastonrobledo@gmail.com>
2019-01-29 16:31:29 -0300

    adding changes to download resources

--- src/lib/stream/section.js
+++ src/lib/stream/section.js
@@ -1,17 +1,20 @@
 const slugify = require('slugify'),
       _ = require('lodash'),
       jp = require('jsonpath'),
+      mime = require('mime'),
+      request = require('request'),
       CONFIG_KEYS = ['app', 'notification', 'policy', 'role', 'smsNumber', 'serviceAccount', 'storage', 'configuration'],
-      MANIFEST_KEYS = ['manifest', 'manifest-dependencies', 'manifest-exports']
+      MANIFEST_KEYS = ['manifest', 'manifest-dependencies', 'manifest-exports'],
+      SectionsCreated = []
 
 class SectionBase {
 
-  constructor(content, key = '', jsNs = 'js') {
+  constructor(content, key = '') {
     this.content = content
     this.key = key
-    this.jsNs = jsNs
-    this.jsInScript = {}
-    this.extractScriptCode()
+    this.scriptFiles = {}
+    this.extraFile = null
+    SectionsCreated.push(this)
     if (new.target === SectionBase) {
       Object.seal(this)
     }
@@ -48,15 +51,44 @@ class SectionBase {
     return this.getParentFromPath(path)
   }
 
-  extractScriptCode() {
+  downloadResources() {
+    return request(this.extraFile.data)
+  }
+
+  replaceFacets() {
+    return new Promise(async(success) => {
+      const nodes = jp.nodes(this.content, '$..path')
+      if (nodes.length) {
+        const parent = this.getParentFromPath(nodes[0].path),
+              facets = _.filter(SectionsCreated, sc => sc.key === 'facet'),
+              facet = _.find(facets, f => parent.path === f.content.name)
+        if (facet) {
+          const { content } = facet
+          this.extraFile = {
+            name: content.name,
+            facet,
+            ext: mime.getExtension(content.mime),
+            data: content.url || content.data,
+            hasToDownload: content.url ? true : false
+          }
+          success()
+        } else {
+          return success()
+        }
+      }
+      return success()
+    })
+  }
+
+  async getScripts() {
     const nodes = jp.nodes(this.content, '$..script')
     nodes.forEach((n) => {
       if (!_.isObject(n.value)) {
         const path = jp.stringify(n.path),
               parent = this.getParentFromPath(n.path),
               name = slugify(parent.code || parent.name || parent.label, '_')
-        this.jsInScript[path] = { value: n.value, name }
-        jp.value(this.content, path, `#${this.jsNs}/${name}.js`)
+        this.scriptFiles[path] = { value: n.value, name }
+        jp.value(this.content, path, `js/${name}.js`)
       }
     })
   }
