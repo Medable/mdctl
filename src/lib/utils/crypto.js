cbe40c5bd14fdad39e361c6a6001d40f9ae4cbab
Gaston Robledo <gastonrobledo@gmail.com>
2019-01-31 20:40:16 -0300

    adding changes to check etag md5, change validations

--- src/lib/utils/crypto.js
+++ src/lib/utils/crypto.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto'),
+      fs = require('fs'),
       { rInt } = require('./values'),
       alphaNumChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
@@ -38,7 +39,28 @@ function randomAlphaNum(length) {
 
 }
 
+function md5FileHash(filename) {
+  const size = 8192,
+        fd = fs.openSync(filename, 'r'),
+        hash = crypto.createHash('md5'),
+        buffer = Buffer.alloc(size)
+
+  try {
+    let bytesRead
+
+    do {
+      bytesRead = fs.readSync(fd, buffer, 0, size)
+      hash.update(buffer.slice(0, bytesRead))
+    } while (bytesRead === size)
+  } finally {
+    fs.closeSync(fd)
+  }
+
+  return hash.digest('hex')
+}
+
 module.exports = {
   secureRandomInt,
-  randomAlphaNum
+  randomAlphaNum,
+  md5FileHash
 }
