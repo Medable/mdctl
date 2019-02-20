a59f091f482b8a0aae7c14f44c9ad861c81e4bb5
james <james@medable.com>
2019-02-20 10:33:56 -0800

    [lib] added strictSSL to client config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const Fault = require('./lib/fault'),
       { Manifest } = require('./lib/manifest'),
       api = require('./lib/api'),
       env = require('./lib/env'),
+      { Config } = require('./lib/config'),
       credentials = require('./lib/credentials')
 
 module.exports = {
@@ -10,7 +11,7 @@ module.exports = {
   api,
   credentials,
   env,
-
+  Config,
   Fault,
   Manifest
 }
