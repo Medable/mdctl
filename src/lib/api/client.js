dce52d7d9d3bac7b3e60c250904c47b75e6867ea
Gaston Robledo <gaston@medable.com>
2019-03-06 17:47:13 -0300

    remove verbose

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -187,7 +187,7 @@ class Client {
     }
 
     return new Promise((resolve, reject) => {
-      req.run(Object.assign({ uri, stream, verbose: true }, requestOptions))
+      req.run(Object.assign({ uri, stream }, requestOptions))
         .then(async(result) => {
 
           if (stream) {
