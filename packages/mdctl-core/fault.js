7039fbf0bc965be70f6260ae09418a9ca80b01af
james <james@medable.com>
2019-04-10 23:21:34 -0700

    [mdctl-core] added support for errCode in faults (for easier multi-lang support)

--- packages/mdctl-core/fault.js
+++ packages/mdctl-core/fault.js
@@ -1,6 +1,36 @@
 const isPlainObject = require('lodash.isplainobject'),
       { pathTo } = require('@medable/mdctl-core-utils'),
-      { rString, rArray } = require('@medable/mdctl-core-utils/values')
+      { rString, rArray } = require('@medable/mdctl-core-utils/values'),
+      faultErrCodeLookup = {
+        kInvalidArgument: 'invalidArgument',
+        kValidationError: 'validation',
+        kAccessDenied: 'accessDenied',
+        kNotFound: 'notFound',
+        kTimeout: 'timeout',
+        kExists: 'exists',
+        kExpired: 'expired',
+        kRequestTooLarge: 'tooLarge',
+        kThrottled: 'throttled',
+        kTooBusy: 'tooBusy',
+        kError: 'error',
+        kNotImplemented: 'notImplemented',
+        kUnsupportedOperation: 'unsupportedOperation'
+      },
+      faultCodeLookup = {
+        invalidArgument: 'kInvalidArgument',
+        validation: 'kValidationError',
+        accessDenied: 'kAccessDenied',
+        notFound: 'kNotFound',
+        timeout: 'kTimeout',
+        exists: 'kExists',
+        expired: 'kExpired',
+        tooLarge: 'kRequestTooLarge',
+        throttled: 'kThrottled',
+        tooBusy: 'kTooBusy',
+        error: 'kError',
+        notImplemented: 'kNotImplemented',
+        unsupportedOperation: 'kUnsupportedOperation'
+      }
 
 class Fault extends Error {
 
@@ -8,16 +38,26 @@ class Fault extends Error {
 
     super()
 
-    Object.assign(this, {
-      faults: [],
-      code,
-      reason,
-      message: message || '',
-      statusCode: statusCode || 500,
-      name: name || 'fault',
-      path,
-      index
+    const obj = Fault.normalizeOptions(
+      code, message, statusCode, name, reason, path, [], null, index
+    )
+
+    this.faults = []
+    this.errCode = obj.errCode
+    this.code = obj.code
+    this.statusCode = obj.statusCode
+    this.name = obj.name
+    this.reason = obj.reason
+    this.path = obj.path
+    this.trace = obj.trace
+    this.index = obj.index
+    this.message = obj.message
+
+    rArray(obj.faults).forEach((f) => {
+      const child = Fault.from(f)
+      if (child) this.add(child)
     })
+
   }
 
   toString() {
@@ -35,6 +75,57 @@ class Fault extends Error {
     return this.reason || ''
   }
 
+  static normalizeOptions(input, msg, statusCode, name, reason, path, faults, trace, index) {
+
+    let obj,
+        errCode,
+        code = input
+
+    if (isPlainObject(code)) {
+      obj = { ...code }
+    } else {
+      if (Fault.isErrCode(code)) {
+        errCode = code
+        code = Fault.errCodeToCode(errCode)
+      } else {
+        errCode = Fault.codeToErrCode(code)
+      }
+      if (isPlainObject(msg)) {
+        obj = { errCode, code, ...msg }
+      } else {
+        obj = {
+          errCode, code, msg, statusCode, name, reason, path, trace, index, faults
+        }
+      }
+    }
+
+    obj.errCode = obj.errCode
+      || (Fault.isErrCode(obj.code) ? obj.code : Fault.codeToErrCode(obj.code))
+    obj.code = obj.code || Fault.errCodeToCode(obj.errCode)
+    obj.message = obj.msg || obj.message
+    obj.statusCode = obj.statusCode || obj.status || 500
+
+    return obj
+
+  }
+
+  static isErrCode(code) {
+    return typeof code === 'string' && code.indexOf('.') !== -1
+  }
+
+  static errCodeToCode(errCode) {
+    if (Fault.isErrCode(errCode)) {
+      const [, code] = errCode.split('.')
+      return faultCodeLookup[code] || 'kError'
+    }
+    return 'kError'
+  }
+
+  static codeToErrCode(code = 'kError', ns = 'mdctl', detail = 'unspecified') {
+    return [ns, faultErrCodeLookup[code] || 'error', detail].join('.')
+
+  }
+
   static from(err, forceError) {
 
     // already a fault of non-convertible?
@@ -48,14 +139,7 @@ class Fault extends Error {
     if (isObject && !(err instanceof Error)) {
       if (pathTo(err, 'object') === 'fault') {
         return Fault.create(
-          err.code,
-          err.message,
-          err.statusCode || err.status || 500,
-          err.name,
-          err.reason,
-          err.path,
-          err.faults,
-          err.index
+          err
         )
       }
     }
@@ -100,6 +184,7 @@ class Fault extends Error {
 Object.assign(Error.prototype, {
 
   object: 'fault',
+  errCode: 'mdctl.error.unspecified',
   code: 'kError',
   name: 'error',
   statusCode: 500,
@@ -109,6 +194,7 @@ Object.assign(Error.prototype, {
     const json = {
       object: 'fault',
       name: this.name || 'error',
+      errCode: this.errCode || 'mdctl.error.unspecified',
       code: this.code || 'kError',
       message: this.getMessage(),
       status: this.statusCode || this.status,
