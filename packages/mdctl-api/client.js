e7b3b4eff54ecf6f7dad4e61055fe2b496b0200f
Gaston Robledo <gaston@medable.com>
2019-09-10 11:07:01 -0300

    updae mdctl-api to use axios instead of request/request

--- packages/mdctl-api/client.js
+++ packages/mdctl-api/client.js
@@ -1,5 +1,6 @@
 
-const request = require('request'),
+const tough = require('tough-cookie'),
+      axios = require('axios'),
       clone = require('clone'),
       { pathTo } = require('@medable/mdctl-core-utils'),
       {
@@ -25,6 +26,9 @@ class Client {
    *  provider: credentials provider || Config.global.credentials.provider
    *  sessions (boolean:false) automatically load and store fingerprints and
    *    session data in the keychain for a login session.
+   *  cancelRequest: Axios token to cancel a request
+   *  onDownloadProgress: function to capture download progress
+   *  onUploadProgress: function to capture upload progress
    */
   constructor(input) {
 
@@ -53,7 +57,7 @@ class Client {
       // for session-based requests
       sessions: rBool(options.sessions),
 
-      cookieJar: request.jar(),
+      cookieJar: new tough.CookieJar(),
 
       // environment endpoint
       environment,
@@ -68,11 +72,20 @@ class Client {
       requestOptions: isSet(options.requestOptions) ? clone(options.requestOptions) : {},
 
       // last response object
-      response: null
+      response: null,
+
+      cancelRequest: options.cancelRequest || (new axios.CancelToken(() => {})),
+      onDownloadProgress: options.onDownloadProgress || (() => {}),
+      onUploadProgress: options.onUploadProgress || (() => {}),
 
     })
 
   }
+
+  get cancelToken () {
+    return axios.CancelToken
+  }
+
 
   get provider() {
     return privatesAccessor(this).provider
@@ -123,6 +136,7 @@ class Client {
    *  json - defaults to true. if true, body must be an object.
    *  cookies - defaults to true. set to false to prevent sending cookies
    *  query - request uri query parameters
+   *  cancelRequest - cancel request token
    *  requestOptions - custom request options, passed directly to the request (https://github.com/request)
    *    strictSSL: default to client strictSSL
    *  stream - pipes the req to the stream and returns (errors and results are not parsed)
@@ -141,7 +155,12 @@ class Client {
               jar: rBool(options.cookies, true) && privates.cookieJar
             },
             privates.requestOptions,
-            options.requestOptions
+            options.requestOptions,
+            {
+              cancelRequest: options.cancelRequest,
+              onDownloadProgress: privates.onDownloadProgress,
+              onUploadProgress: privates.onUploadProgress
+            }
           ),
           req = new Request(),
           basic = rBool(options.basic),
@@ -160,7 +179,7 @@ class Client {
             session = await privates.provider.getCustom('session', environment.url)
 
       if (fingerprint) {
-        requestOptions.jar.setCookie(fingerprint, environment.endpoint)
+        requestOptions.jar.setCookieSync(fingerprint, environment.endpoint)
       }
 
       if (session) {
@@ -168,7 +187,7 @@ class Client {
           privates.csrfToken = session.csrf
         }
         if (session.sid) {
-          requestOptions.jar.setCookie(session.sid, environment.url)
+          requestOptions.jar.setCookieSync(session.sid, environment.url)
         }
       }
 
@@ -207,7 +226,7 @@ class Client {
 
             // store the latest fingerprint in the keychain for this endpoint.
             try {
-              const fingerprint = requestOptions.jar.getCookies(environment.endpoint).filter(
+              const fingerprint = requestOptions.jar.getCookiesSync(environment.endpoint).filter(
                 cookie => cookie.key === 'md.fingerprint' && cookie.path === '/'
               )[0]
 
@@ -238,7 +257,7 @@ class Client {
             const existing = await privates.provider.getCustom('session', environment.url),
                   session = {
                     csrf: response.headers['medable-csrf-token'],
-                    sid: rVal(requestOptions.jar.getCookies(environment.url).filter(
+                    sid: rVal(requestOptions.jar.getCookiesSync(environment.url).filter(
                       cookie => cookie.key === 'md.sid' && cookie.path === `/${environment.env}`
                     )[0], '').toString()
                   }
