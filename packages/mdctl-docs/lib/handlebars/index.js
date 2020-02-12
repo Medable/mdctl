fa2bef36f1e0eccb1befe70b713ade525c5f2ae1
Adam Coulon <adam_hg@medable.com>
2020-02-12 16:05:11 -0600

    completed generation of env/ docs

--- packages/mdctl-docs/lib/handlebars/index.js
+++ packages/mdctl-docs/lib/handlebars/index.js
@@ -6,22 +6,23 @@ const Handlebars = require('handlebars'),
         'gitbook/route',
         'gitbook/nav-item',
         'gitbook/tab',
-        'md/app',
         'md/function',
         'md/key-value',
-        'md/list',
+        'md/resource',
+        'md/set',
         'md/object',
-        'md/value',
+        'md/parameters',
+        'md/value'
       ]),
 
       TEMPLATES = Object.freeze({
         GITBOOK: {
           INTRODUCTION: Handlebars.compile(load('gitbook/introduction')),
           MODULE: Handlebars.compile(load('gitbook/module')),
-          SUMMARY: Handlebars.compile(load('gitbook/summary')),
+          SUMMARY: Handlebars.compile(load('gitbook/summary'))
         },
         MD: {
-          APPS: Handlebars.compile(load('md/apps')),
+          RESOURCES: Handlebars.compile(load('md/resources'))
         }
       })
 
@@ -29,15 +30,19 @@ function load(component) {
   return Fs.readFileSync(Path.join(__dirname, 'components', `${component}.hbs`), 'utf8')
 }
 
+Handlebars.registerHelper('cammel_to_sentence', s => (typeof s === 'string' ? s.replace(/([A-Z])/g, match => ` ${match}`).replace(/^./, match => match.toUpperCase()) : s))
+
 /**
  * TODO: Improve REGEX
  *   This REGEX is sloppy and needs to more closely represent markdown formatting.
  *   It should consider space around the characters, formatting pairs (open/close),
  *   already escaped, etc
  */
-Handlebars.registerHelper('escape_md', s => (typeof s === 'string' ? s.replace(/(\\|\*|_|`|{|}|\(|\)|\[|\]|#)/g, '\\$1') : s))
+Handlebars.registerHelper('md_escape', s => (typeof s === 'string' ? s.replace(/(\\|\*|_|`|{|}|\(|\)|\[|\]|#)/g, '\\$1') : s))
 
-Handlebars.registerHelper('cammel_to_sentence', s => (typeof s === 'string' ? s.replace(/([A-Z])/g, match => ` ${match}`).replace(/^./, match => match.toUpperCase()) : s))
+Handlebars.registerHelper('md_header', (s, level=1) => (typeof s === 'string' ? `${'#'.repeat(level)} ${s}` : s))
+
+Handlebars.registerHelper('new_level', (current=1, delta=0) => current + delta)
 
 PARTIALS.forEach(partial => Handlebars.registerPartial(partial.replace(/\//g, '.'), load(partial)))
 
