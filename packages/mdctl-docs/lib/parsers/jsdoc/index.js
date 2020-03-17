c2d13cb04e6d71a7da4d4e71681e579600e4b530
Adam Coulon <adam_hg@medable.com>
2020-03-17 10:55:30 -0500

    checking multiple potential locations for jsdoc

--- packages/mdctl-docs/lib/parsers/jsdoc/index.js
+++ packages/mdctl-docs/lib/parsers/jsdoc/index.js
@@ -1,15 +1,37 @@
 const { spawn } = require('child_process'),
       Path = require('path'),
-      jsdoc = Path.join(__dirname, '../../../../..', '.bin', 'jsdoc')
+      Util = require('../../util')
+
+function locateJsdoc() {
+
+  let path = __dirname,
+      jsdoc
+
+  while (path) {
+    const jsdocPathPotential = Path.join(path, 'node_modules', '.bin', 'jsdoc')
+    if (!jsdoc && Util.isExecutable(jsdocPathPotential)) {
+      jsdoc = jsdocPathPotential
+    }
+    path = path.substr(0, path.lastIndexOf('/'))
+  }
+
+  if (!jsdoc) {
+    console.log(__dirname)
+    throw new Error('Unable to find JSDoc executable')
+  }
+
+  return jsdoc
+}
 
 function getAst(options) {
   return new Promise((resolve, reject) => {
     const params = [
-      options.source,
-      '--recurse',
-      '--configure', Path.join(__dirname, 'config.json'),
-      '--explain' // Dump all doclets to the console in JSON format, then exit.
-    ]
+            options.source,
+            '--recurse',
+            '--configure', Path.join(__dirname, 'config.json'),
+            '--explain' // Dump all doclets to the console in JSON format, then exit.
+          ],
+          jsdoc = locateJsdoc()
 
     // FIXME: These are currently not compatible with --explain
     // if (options.verbose) {
@@ -25,7 +47,9 @@ function getAst(options) {
       proc.stdout.on('data', (data) => {
         result.push(data.toString('utf8'))
       })
-      proc.stderr.on('data', console.log)
+      proc.stderr.on('data', (data) => {
+        console.log(data.toString('utf8'))
+      })
       proc.on('close', () => {
         resolve(JSON.parse(result.join('')))
       })
