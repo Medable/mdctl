9bbb20d712f4fa757e9ad8cd9b13e86ee7357e6a
Nahuel Dealbera <nahuel.dealbera@gmail.com>
2021-12-14 13:26:58 -0300

    TOOLS-42: Added more tests

--- packages/mdctl-axon-tools/__tests__/TOOLS-42/TOOLS-42.test.js
+++ packages/mdctl-axon-tools/__tests__/TOOLS-42/TOOLS-42.test.js
@@ -1,7 +1,12 @@
 // eslint-disable-next-line import/no-dynamic-require
-const Transform = require(`${__dirname}/../../packageScripts/ingestTransform.js`)
+const fs = require('fs')
+const Transform = require('../../packageScripts/ingestTransform.js')
+const StudyManifestTools = require('../../lib/StudyManifestTools')
 
 jest.mock('runtime.transform', () => ({ Transform: class {} }), { virtual: true })
+jest.mock('@medable/mdctl-core-utils/privates', () => ({ privatesAccessor: () => ({ options: { dir: __dirname } }) }), { virtual: true })
+jest.mock('@medable/mdctl-api-driver', () => ({ Driver: class {} }), { virtual: true })
+jest.mock('@medable/mdctl-api-driver/lib/cortex.object', () => ({ Object: class {} }), { virtual: true })
 
 describe('ingestTransform', () => {
 
@@ -86,4 +91,56 @@ describe('ingestTransform', () => {
   })
 
 
+})
+
+describe('StudyManifestTools', () => {
+
+  const ingestTransform = 'ingestTransform.js',
+        ingestTransformPath = `${__dirname}/${ingestTransform}`,
+        packageJson = 'package.json',
+        packageJsonPath = `${__dirname}/${packageJson}`
+
+  afterEach(() => {
+    if (fs.existsSync(ingestTransformPath)) {
+      fs.unlinkSync(ingestTransformPath)
+    }
+
+    if (fs.existsSync(packageJsonPath)) {
+      fs.unlinkSync(packageJsonPath)
+    }
+  })
+
+  it.each([
+    [
+      'study',
+      {
+        object: 'package', name: 'Study export', version: '0.0.1', description: 'An export of a study', pipes: { ingest: 'ingestTransform.js' }
+      }
+    ],
+    [
+      'task',
+      {
+        object: 'package', name: 'Task export', version: '0.0.1', description: 'An export of task or multiple tasks', pipes: { ingest: 'ingestTransform.js' }
+      }
+    ],
+    [
+      'consent',
+      {
+        object: 'package', name: 'Consent export', version: '0.0.1', description: 'An export of task or multiple consent templates', pipes: { ingest: 'ingestTransform.js' }
+      }
+    ],
+  ])('should writePackage for: %s', (packageType, expected) => {
+    const studyManifestTools = new StudyManifestTools()
+
+    studyManifestTools.writePackage(packageType)
+
+    expect(fs.existsSync(ingestTransformPath)).toBeTruthy()
+    expect(fs.existsSync(packageJsonPath)).toBeTruthy()
+
+    // eslint-disable-next-line one-var
+    const copiedPackageJson = JSON.parse(fs.readFileSync(packageJsonPath))
+
+    expect(copiedPackageJson)
+      .toStrictEqual(expected)
+  })
 })
