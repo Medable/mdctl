3389cb361cb174ede1157c1f6fbe5b6c244f4d94
Nahuel Dealbera <nahuel.dealbera@gmail.com>
2022-04-15 18:03:30 -0300

    MIG-9 Added tests

--- packages/mdctl-axon-tools/__tests__/MIG-63/MIG-63.test.js
+++ packages/mdctl-axon-tools/__tests__/MIG-63/MIG-63.test.js
@@ -1,4 +1,5 @@
 const MenuConfigMap = require('../../lib/mappings/maps/MenuConfigMap')
+const ReviewsTypesMap = require('../../lib/mappings/maps/ReviewTypesMap')
 const MenuConfigMapping = require('../../lib/mappings/MenuConfigMapping')
 
 describe('MenuConfigMappings', () => {
@@ -18,6 +19,35 @@ describe('MenuConfigMappings', () => {
                         c_menu_config: [{
                           c_key: 'bbb',
                           c_group_id: '001',
+                        }],
+                        c_review_types: [{
+                          _id: 'rrr',
+                          c_key: 'bbb',
+                          c_active: true,
+                          c_roles: ['xyz']
+                        }]
+                      }]
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+      org: {
+        find() {
+          return {
+            paths() {
+              return {
+                limit() {
+                  return {
+                    async toArray() {
+                      return [{
+                        roles: [{
+                          _id: 'xyz',
+                          c_key: 'abc',
+                          code: 'admin'
                         }]
                       }]
                     }
@@ -84,6 +114,23 @@ describe('MenuConfigMappings', () => {
           }
         }
       },
+      c_group_tasks: {
+        find() {
+          return {
+            paths() {
+              return {
+                async toArray() {
+                  return [{
+                    _id: 'fff',
+                    c_key: 'ppp',
+                    c_required_reviews: ['rrr']
+                  }]
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
 
@@ -123,7 +170,7 @@ describe('MenuConfigMappings', () => {
           expectedScript = `
 const { run } = require('expressions')
 
-const mappings = [{"path":"c_study.aaa.c_menu_config.bbb.c_group_id","mapTo":{"$pathTo":[{"$dbNext":{"object":"c_group","operation":"cursor","paths":["_id"],"where":{"c_key":"ccc"}}},"_id"]}}]
+const mappings = [{"path":"c_study.aaa.c_menu_config.bbb.c_group_id","mapTo":{"$pathTo":[{"$dbNext":{"object":"c_group","operation":"cursor","paths":["_id"],"where":{"c_key":"ccc"}}},"_id"]}},{"path":"c_study.aaa.c_review_types.bbb.c_roles","mapTo":{"$dbNext":{"expressionPipeline":[{"$transform":{"each":{"in":{"$map":{"as":"role","in":{"$pathTo":["$$role","_id"]},"input":{"$filter":{"as":"role","cond":{"$in":["$$role.code",{"$array":[{"$literal":"admin"}]}]},"input":"$$ROOT.roles"}}}}}}}],"maxTimeMS":10000,"object":"org","operation":"cursor","paths":["roles"]}}},{"path":"c_group_task.ppp.c_required_reviews","mapTo":{"$dbNext":{"expressionPipeline":[{"$transform":{"each":{"in":{"$map":{"as":"reviewType","in":"$$reviewType._id","input":{"$filter":{"as":"reviewType","cond":{"$in":["$$reviewType.c_key",{"$array":[{"$literal":"bbb"}]}]},"input":"$$ROOT.c_review_types"}}}}}}}],"maxTimeMS":10000,"object":"c_study","operation":"cursor"}}}]
 
 mappings.forEach(({ path, mapTo }) => {
 
@@ -167,5 +214,118 @@ mappings.forEach(({ path, mapTo }) => {
 
     expect(script)
       .toBe(expectedScript)
+  })
+
+  describe('MIG-9: Review types mapping', () => {
+
+    it('getStudyReviewMaps', async() => {
+      const mapping = new ReviewsTypesMap(org),
+            mappings = await mapping.getStudyReviewMaps()
+
+      expect(mappings)
+        .toStrictEqual([
+          {
+            path: 'c_study.aaa.c_review_types.bbb.c_roles',
+            mapTo: {
+              $dbNext: {
+                expressionPipeline: [
+                  {
+                    $transform: {
+                      each: {
+                        in: {
+                          $map: {
+                            as: 'role',
+                            in: {
+                              $pathTo: [
+                                '$$role',
+                                '_id'
+                              ]
+                            },
+                            input: {
+                              $filter: {
+                                as: 'role',
+                                cond: {
+                                  $in: [
+                                    '$$role.code',
+                                    {
+                                      $array: [
+                                        {
+                                          $literal: 'admin'
+                                        }
+                                      ]
+                                    }
+                                  ]
+                                },
+                                input: '$$ROOT.roles'
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                ],
+                maxTimeMS: 10000,
+                object: 'org',
+                operation: 'cursor',
+                paths: [
+                  'roles'
+                ]
+              }
+            }
+          }
+        ])
+    })
+
+    it('getStudyReviewMaps', async() => {
+      const mapping = new ReviewsTypesMap(org),
+            mappings = await mapping.getGroupTaskReviewMaps()
+
+      expect(mappings)
+        .toStrictEqual([
+          {
+            path: 'c_group_task.ppp.c_required_reviews',
+            mapTo: {
+              $dbNext: {
+                expressionPipeline: [
+                  {
+                    $transform: {
+                      each: {
+                        in: {
+                          $map: {
+                            as: 'reviewType',
+                            in: '$$reviewType._id',
+                            input: {
+                              $filter: {
+                                as: 'reviewType',
+                                cond: {
+                                  $in: [
+                                    '$$reviewType.c_key',
+                                    {
+                                      $array: [
+                                        {
+                                          $literal: 'bbb'
+                                        }
+                                      ]
+                                    }
+                                  ]
+                                },
+                                input: '$$ROOT.c_review_types'
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                ],
+                maxTimeMS: 10000,
+                object: 'c_study',
+                operation: 'cursor'
+              }
+            }
+          }
+        ])
+    })
   })
 })
