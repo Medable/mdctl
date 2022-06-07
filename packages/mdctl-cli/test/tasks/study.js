94a8df15bb9418b47593403885ff4bf6f678fdd4
Davide Aquaro <davide.aquaro@medable.com>
2022-06-07 15:37:57 +0100

    feat: ignore keys in manifest other than tasks and templates

--- packages/mdctl-cli/test/tasks/study.js
+++ packages/mdctl-cli/test/tasks/study.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { assert } = require('chai'),
+const { assert, expect } = require('chai'),
       Study = require('../../tasks/study')
 
 let study
@@ -43,6 +43,65 @@ describe('MIG-85 - Test partial migrations in study.js module', () => {
       .equal(res.code, 'kInvalidArgument')
     assert
       .equal(res.reason, 'The manifest is not a valid JSON')
+  })
+
+  it('Test validateManifest function - No entities to export', () => {
+    const manifest = `{
+      "object": "manifest",
+      "c_group": {
+        "includes": [
+          "key-014"
+        ]
+      },
+      "c_group_task": {
+        "includes": [
+          "key-015"
+        ]
+      }
+    }`
+
+    let res
+    try {
+      res = study.validateManifest(manifest)
+    } catch (err) {
+      res = err
+    }
+
+    assert
+      .equal(res.errCode, 'mdctl.invalidArgument.unspecified')
+    assert
+      .equal(res.code, 'kInvalidArgument')
+    assert
+      .equal(res.reason, 'No Assignments or eConsents to export')
+
+  })
+
+  it('Test validateManifest function - Removed entities other than Assignments and eConsents', () => {
+    const manifest = `{
+            "object": "manifest",
+            "c_task": {
+              "includes": [
+                "key-001"
+              ]
+            },
+            "c_group_task": {
+              "includes": [
+                "key-002"
+              ]
+            }
+          }`,
+          res = study.validateManifest(manifest)
+
+    expect(res)
+      .to.deep.equal({
+        object: 'manifest',
+        c_task: {
+          includes: [
+            'key-001'
+          ]
+        }
+      })
+
   })
 
 })
