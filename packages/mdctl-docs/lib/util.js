1eb71de2abf120c9effbb2f1576bb53e89f5d60e
Adam Coulon <adam.coulon@medable.com>
2023-01-04 23:11:01 -0600

    Refactor Docs Task

--- packages/mdctl-docs/lib/util.js
+++ packages/mdctl-docs/lib/util.js
@@ -5,7 +5,6 @@ const path = require('path')
  * Function
  */
 function noop() {
-
 }
 
 /**
@@ -15,12 +14,28 @@ function capitalizeFirstCharacter(s) {
   return `${s.charAt(0).toUpperCase()}${s.slice(1)}`
 }
 
+function cammelToSentence(s){
+  return typeof s === 'string'
+    ? s.replace(/([A-Z])/g, match => ` ${match}`).replace(/^./, match => match.toUpperCase())
+    : s
+}
+
 function removeExtention(s) {
-  return typeof s === 'string' ? s.replace(/\.[^/.]+$/, '') : s
+  return typeof s === 'string'
+    ? s.replace(/\.[^/.]+$/, '')
+    : s
 }
 
 function capitalize(s) {
-  return typeof s === 'string' ? s.toUpperCase() : s
+  return typeof s === 'string'
+    ? s.toUpperCase()
+    : s
+}
+
+function stringify(objOrArray, replacer, space) {
+  return isObject(objOrArray) || Array.isArray(objOrArray)
+    ? JSON.stringify(objOrArray, replacer, space)
+    : objOrArray
 }
 
 /**
@@ -28,6 +43,10 @@ function capitalize(s) {
  */
 function clone(obj) {
   return JSON.parse(JSON.stringify(obj))
+}
+
+function isObject(obj){
+  return Object.prototype.toString.call(obj) === '[object Object]'
 }
 
 function array2Obj(array, opts) {
@@ -49,73 +68,19 @@ function array2Obj(array, opts) {
   return obj
 }
 
-function determineResourceLevel(rawResource, level = 1) {
-  return rawResource.label || rawResource.name
-    ? level + 1
-    : level
-}
-
-function breakdownResource(rawResource, level = 1) {
-  const scripts = [],
-        sets = [],
-        resources = [],
-        properties = [],
-        label = rawResource.label
-          || rawResource.name
-
-  Object.entries(rawResource).forEach(([key, value]) => {
-    if (value !== null) {
-      if (typeof value === 'object') {
-        const nextLevel = level + 1
-        if (Array.isArray(value)) {
-          if (['[object Object]', '[object Array]'].includes(Object.prototype.toString.call(value[0]))) {
-            resources.push({
-              label: key,
-              level: nextLevel,
-              resources: value.map(rawSubResource => breakdownResource(
-                rawSubResource,
-                determineResourceLevel(rawSubResource, nextLevel)
-              ))
-            })
-          } else if (!(value[0] == null)) {
-            sets.push({ key, value })
-          }
-        } else {
-          resources.push({
-            ...breakdownResource(value, determineResourceLevel(value, nextLevel)),
-            label: key
-          })
-        }
-      } else if (key === 'script') {
-        const isScriptText = false
-        if (isScriptText) {
-          scripts.push({
-            code: value,
-            language: 'javascript'
-          })
-        }
-      } else if (key !== 'label') {
-        properties.push({ key, value })
-      }
-    }
-  })
-
-  return {
-    scripts,
-    sets,
-    properties,
-    label,
-    level,
-    resources,
-  }
-}
-
 /**
  * Files
  */
 
 function read(file, encoding = 'utf8') {
   return fs.readFileSync(file, encoding)
+}
+
+function listFiles(directory){
+  return fs
+    .readdirSync(directory, { withFileTypes: true })
+    .filter((dirent) => dirent.isFile())
+    .map((dirent) => path.join(directory, dirent.name))
 }
 
 function readJson(path, empty = {}) {
@@ -127,17 +92,17 @@ function readJson(path, empty = {}) {
   }
 }
 
-function writeFiles(files, location) {
-  for (let i = 0; i < files.length; i += 1) {
-    writeFile(files[i], location)
+function writeFiles(files, destination) {
+  for (file of files){
+    writeFile(file, destination)
   }
 }
 
-function writeFile(file, location) {
-  const filePath = file.path ? path.join(location, file.path) : location,
-        fileLocation = path.join(filePath, file.name)
-  ensureDir(filePath)
-  fs.writeFileSync(fileLocation, file.content)
+function writeFile(file, destination) {
+  const directory = path.join(destination, file.path),
+        filePath = path.join(directory, file.name)
+  ensureDir(directory)
+  fs.writeFileSync(filePath, file.content)
 }
 
 function ensureDir(directory) {
@@ -156,75 +121,18 @@ function ensureDir(directory) {
   }
 }
 
-function isExecutable(file) {
-  try {
-    fs.accessSync(file, fs.constants.X_OK)
-  } catch (err) {
-    return false
-  }
-  return true
-}
-
-/**
- * Template
- */
-function compareParams(a, b) {
-  const nameA = a.name.toLowerCase(),
-        nameB = b.name.toLowerCase()
-  if (nameA < nameB) {
-    return -1
-  }
-  if (nameA > nameB) {
-    return 1
-  }
-  return 0
-}
-
-function translateParams(params) {
-  return params
-    .sort(compareParams)
-    .reduce((formattedParams, param) => {
-      const uri = param.name.split('.')
-
-      let list = formattedParams,
-          name = uri.shift()
-      while (name) {
-        if (uri.length === 0) {
-          list.push({
-            name,
-            typeString: param.type
-              ? param.type.names.join('|')
-              : undefined,
-            description: param.description,
-            children: []
-          })
-        } else {
-          const target = findParam(list, name)
-          if (!target) {
-            break
-          }
-          list = target.children
-        }
-        name = uri.shift()
-      }
-      return formattedParams
-    }, [])
-}
-
-function findParam(params, name) {
-  return params.find(param => param.name === name)
-}
-
 module.exports = {
   array2Obj,
-  breakdownResource,
   capitalize,
   capitalizeFirstCharacter,
+  cammelToSentence,
   clone,
-  isExecutable,
+  isObject,
   read,
+  listFiles,
   readJson,
   removeExtention,
-  translateParams,
+  stringify,
   writeFiles,
+  writeFile
 }
