7bfefce2e13435c1549eec3aada0e55dc10f833f
jacobi petrucciani <jacobi.petrucciani@medable.com>
2023-01-12 10:01:02 -0500

    attempt to fixup release workflows, update nix env

--- default.nix
+++ default.nix
@@ -1,63 +1,60 @@
-with builtins;
-{ pkgs ? import
-    (
-      fetchTarball {
-        name = "nixpkgs-unstable-2022-05-04";
-        url = "https://github.com/NixOS/nixpkgs/archive/ccb9c0e53779ec8995d22b26cbc792ad13f8d503.tar.gz";
-        sha256 = "08kplx00lkxg78zf8idfaw30mxrqc2a0f4da342f7hhf32xg41jk";
-      }
-    )
-    {
-      config = {
-        allowUnfree = true;
-      };
-      overlays = [ ];
-    }
+{ medable ? import
+    (fetchTarball {
+      name = "medable-2023-01-12";
+      url = "https://github.com/Medable/nix/archive/611ef8d5bcbf2a4d2fa7441907fec8d017512ae3.tar.gz";
+      sha256 = "08qgscc8w4nzjclp78w4cvli8aw2pr278l578d62v6vgl7gy38jk";
+    })
+    { }
+, pkgs ? medable.pkgs
+, jacobi ? medable.jacobi
 }:
 let
-  inherit (pkgs.lib) flatten;
+  node = jacobi.nodejs-16_x;
 
-  node = pkgs.nodejs-14_x;
-
-  tools = with pkgs; {
+  tools = with jacobi; {
     cli = [ ];
     deps = [
       gcc
       glib
       gnumake
       libsecret
-      nodePackages.node-gyp
-      nodePackages.node-pre-gyp
       pkg-config
       sqlite
     ];
     nix = [
       nixpkgs-fmt
     ];
-    node = [
-      node
-      node.pkgs.lerna
+    npm = with node.pkgs; [
+      lerna
+      node-gyp
+      node-pre-gyp
     ];
     osSpecific = with pkgs.darwin.apple_sdk.frameworks; if pkgs.stdenv.isDarwin then [
       Security
       AppKit
       xcbuild
     ] else [ ];
-    scripts = [
-      (writeShellScriptBin "_npm_setup" ''
-        ${node}/bin/npm install --ci
-        ${node.pkgs.lerna}/bin/lerna bootstrap --hoist
-      '')
-      (writeShellScriptBin "_npm_publish" ''
-        ${node.pkgs.lerna}/bin/lerna version
-        ${node.pkgs.lerna}/bin/lerna publish from-package --dist-tag=@alpha
-      '')
-    ];
+    scripts =
+      let
+        npm = "${node}/bin/npm";
+        lerna = "${node.pkgs.lerna}/bin/lerna";
+      in
+      [
+        (writeShellScriptBin "_npm_setup" ''
+          ${npm} install --ci
+        '')
+        (writeShellScriptBin "_npm_publish_alpha" ''
+          ${lerna} publish from-package --dist-tag=alpha "$@"
+        '')
+        (writeShellScriptBin "_npm_publish_latest" ''
+          ${lerna} publish from-package --dist-tag=latest "$@"
+        '')
+      ];
   };
 
   shell = pkgs.mkShell {
     name = "mdctl";
-    packages = flatten [ (flatten (attrValues tools)) ];
+    packages = jacobi._toolset tools;
   };
 in
 shell
